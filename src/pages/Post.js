7f2db558396c478624819bb88ee995e393ba2beb
Rafiul29 <rafiulislam4472@gmail.com>
2022-06-27 20:25:16 +0600

    react add fg

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -2,6 +2,7 @@ import React from "react";
 function Post(){
     return(
             <>
+
                 <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
                     <div className="container px-4 px-lg-5">
                         <a className="navbar-brand" href="index.html">
