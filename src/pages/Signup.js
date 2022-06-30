87387bb19b29167f7c9891aaee098b5376483fb8
Rafiul29 <rafiulislam4472@gmail.com>
2022-07-01 00:18:20 +0600

    create account signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,34 @@
 import React from "react";
+import axios from 'axios';
+import { useState } from 'react';
 
 function Signup(){
+    const [user_name, setUsername]=useState(" ");
+    const [email, setEmail]=useState(" ");
+    const [first_name, setFirst_name]=useState(" ");
+    const [last_name, setLast_name]=useState(" ");
+    const [password, setPassword]=useState(" ");
+    const [created_at, setCreated_at]=useState(" ");
+    const [country_code, setCountry_code]=useState(" ");
+
+    const Save=async (e)=>{
+        console.log("hello");
+        await axios.post("http://localhost:5000/signup",{
+            user_name,
+            email,
+            first_name,
+            last_name,
+            password,
+            created_at,
+            country_code
+        },{headers: {'Accept': 'application/json',
+                'Content-Type': 'application/json'}})
+            .then((data)=>{
+            console.log(data)
+        })
+
+    }
+
     return(
         <>
             <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
@@ -75,7 +103,169 @@ function Signup(){
             {/*        </div>*/}
             {/*    </div>*/}
             </header>
-</>
+            <div className="container">
+                <div className="row">
+                    <form className="form-horizontal">
+                        <fieldset>
+                            {/* Form Name */}
+                            <legend>Create Account</legend>
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="full_name">
+                                  User Name
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id=" User name"
+                                        name=" User name"
+                                        placeholder=" User name"
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setUsername(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+
+
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="email">
+                                    Email
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        placeholder="Enter Your Email."
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setEmail(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="father_name">
+                                   First Name
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="first_name"
+                                        name="first_name"
+                                        placeholder="Enter Your First Name"
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setFirst_name(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="mother_name">
+                                    Last Name
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="last_name"
+                                        name="last_name"
+                                        placeholder="Enter your Last Name"
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setLast_name(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="mother_name">
+                                    Password
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="last_name"
+                                        name="last_name"
+                                        placeholder="Enter your Password"
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setPassword(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="mother_name">
+                                    Cerate Date
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="last_name"
+                                        name="last_name"
+                                        placeholder="Enter your Last Name"
+                                        className="input-xlarge"
+                                        type="date"
+                                        onChange={(e)=>{
+                                            setCreated_at(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+
+                            {/* Text input*/}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="mobile">
+                                   Country Code
+                                </label>
+                                <div className="controls">
+                                    <input
+                                        id="mobile"
+                                        name="mobile"
+                                        placeholder="Enter Your Country Code"
+                                        className="input-xlarge"
+                                        type="text"
+                                        onChange={(e)=>{
+                                            setCountry_code(e.target.value);
+                                        }}
+                                    />
+                                    <p className="help-block">Error</p>
+                                </div>
+                            </div>
+
+                            {/* Button */}
+                            <div className="control-group">
+                                <label className="control-label" htmlFor="submit" />
+                                <div className="controls">
+                                    <button id="submit" name="submit"  onClick={()=>Save()}   className="btn btn-primary">
+                                        Submit
+                                    </button>
+
+
+
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+
+        </>
     )
 }
 
